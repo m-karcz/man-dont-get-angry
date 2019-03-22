741baea9fcae204051cef70b8027324f44fb492a
Mariusz Karcz <mariusz0karcz@gmail.com>
2019-03-22 19:31:44 +0000

    adding forgotten files

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
- 
+#include "Board.hpp"
+#include "Dispatcher.hpp"
+#include "BoardPrinter.hpp"
+#include "GetBoardMessage.hpp"
+
+int main()
+{
+    Board board = FourPlayersBoardBuilder{}.build();
+
+    Dispatcher dispatcher;
+
+    //dispatcher.subscribe<GetBoardMessage>([&](std::shared_ptr<GetBoardMessage> msg){msg->commit(board);});
+    dispatcher.subscribe(interestByReactOn<GetBoardMessage>().with([&](std::shared_ptr<GetBoardMessage> msg){msg->commit(board);}));
+
+    BoardPrinter printer{dispatcher};
+
+
+    std::cout << "reading state" << std::endl;
+    printer.readState();
+    std::cout << "read state" << std::endl;
+    printer.print();
+}
